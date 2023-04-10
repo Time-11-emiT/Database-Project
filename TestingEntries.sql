cad7c7af2a75985ad257085c9bb80a4cd535b3b1
ShubhamGupta986725 <95865181+ShubhamGupta986725@users.noreply.github.com>
2023-04-10 19:05:59 +0530

    Create TestingEntries.sql

---
+++ TestingEntries.sql
@@ -0,0 +1,4 @@
+
+CALL insertInvestment(1,'Lakshit', 'Sethi', 'lakshit@gmai.com','Sethi', 1, 'laks', 'APPL', 'Stock', 10, '2023-04-10', 35.52, 10.05, 7262.92,689,38,292);
+CALL updateNumberOfShares(1, 50);
+CALL DeleteInvestment(1);
