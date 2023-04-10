0d16faadb9b5029087f8c24c541d2dfa9b23e33e
Time-11-emiT <99984506+Time-11-emiT@users.noreply.github.com>
2023-04-10 19:44:49 +0530

    Update TestingEntries.sql

--- TestingEntries.sql
+++ TestingEntries.sql
@@ -1,5 +1,6 @@
 
 CALL insertInvestment(1,'Lakshit', 'Sethi', 'lakshit@gmai.com','Sethi', 1, 'laks', 'APPL', 'Stock', 10, '2023-04-10', 35.52, 10.05, 7262.92,689,38,292);
+CALL insertInvestment(2, 'John', 'Doe', 'john.doe@gmail.com', 'Doe', 2, 'jdoe', 'MSFT', 'Stock', 15, '2023-04-10', 285.05, 8.23, 42814.77, 1201, 62, 357);
 CALL updateNumberOfShares(1, 50);
 CALL DeleteInvestment(1);
 CALL getTotalReturns();
