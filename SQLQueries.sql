9316cf3115825f26d6a0185e61004273f6410aaf
ShubhamGupta986725 <95865181+ShubhamGupta986725@users.noreply.github.com>
2023-04-11 22:05:43 +0530

    Update SQLQueries.sql

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -1,5 +1,21 @@
+DROP procedure IF EXISTS createInvestor;
+DROP procedure IF EXISTS createPortfolio;
 DROP procedure IF EXISTS insertInvestment;
 DROP procedure IF EXISTS updateNumberOfShares;
+DROP procedure IF EXISTS deleteInvestment;
+DROP procedure IF EXISTS getTotalReturns;
+DROP procedure IF EXISTS getStockPrices;
+DROP procedure IF EXISTS getAverageAnnualizedReturn;
+DROP procedure IF EXISTS filterByRiskLevel;
+DROP procedure IF EXISTS findTotalValueOfAllInvestments;
+DROP procedure IF EXISTS findOverallAnnualizedReturns;
+DROP procedure IF EXISTS findCorrelation;
+DROP procedure IF EXISTS getInflationRate;
+DROP procedure IF EXISTS filterStockPrice;
+DROP procedure IF EXISTS percentageChangeInStockPrice;
+DROP procedure IF EXISTS calculateVolatility;
+DROP procedure IF EXISTS getTopPerformingStocks;
+DROP procedure IF EXISTS numberOfSharesForEachInvestmentType;
 
 
 DELIMITER $$
@@ -61,19 +77,18 @@ DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE updateNumberOfShares(
-	IN p_investmentID INT,
-	IN new_num_shares INT
+IN p_investmentID INT,
+IN new_num_shares INT
 )
 BEGIN
-	-- Update the investment in the Investment table where InvestmentID = p_investmentID
-	update investment SET investment.NumShares = new_num_shares WHERE investment.InvestmentID=p_investmentID;
+-- Update the investment in the Investment table where InvestmentID = p_investmentID
+update investment SET investment.NumShares = new_num_shares WHERE investment.InvestmentID=p_investmentID;
 END$$
 DELIMITER ;
 
-
 DELIMITER $$
-CREATE PROCEDURE DeleteInvestment(
-	IN p_investmentID INT
+CREATE PROCEDURE DeleteInvestment (
+IN p_investmentID INT
 )
 BEGIN
 	-- Delete the investment in the Market_Data table where InvestmentID = p_investmentID
@@ -96,11 +111,11 @@ DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE getStockPrices(
-	IN p_Date DATE
+  IN p_Date DATE
 )
 BEGIN
-	-- Join the Investment and Market_Data tables and retrieve the StockPrice column for the specified date
-	SELECT i.InvestmentName, md.StockPrice FROM Investment i JOIN Market_Data md ON i.InvestmentID = md.InvestmentID WHERE md.Date = p_Date;
+  -- Join the Investment and Market_Data tables and retrieve the StockPrice column for the specified date
+SELECT i.InvestmentName, md.StockPrice FROM Investment i JOIN Market_Data md ON i.InvestmentID = md.InvestmentID WHERE md.Date = p_Date;
 
 END$$
 DELIMITER ;
@@ -115,7 +130,7 @@ DELIMITER ;
 DELIMITER $$
 CREATE PROCEDURE filterByRiskLevel()
 BEGIN
-	SELECT i.InvestmentName, pm.TotalReturn, pm.AnnualizedReturn FROM Investment i JOIN Performance_Metrics pm ON i.InvestmentID = pm.InvestmentID ORDER BY pm.AnnualizedReturn DESC LIMIT 10;
+	SELECT i.InvestmentName, pm.TotalReturn, pm.AnnualizedReturn FROM Investment i JOIN Performance_Metrics pm ON i.InvestmentID = pm.InvestmentID ORDER BY pm.RiskLevel DESC LIMIT 10;
 END$$
 DELIMITER ;
 
@@ -135,86 +150,53 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE CalculateCorrelation(IN id1 INT, IN id2 INT)
-     BEGIN
-         DECLARE x FLOAT;
-         DECLARE y FLOAT;
-         DECLARE x_sum FLOAT;
-         DECLARE y_sum FLOAT;
-         DECLARE x_squared_sum FLOAT;
-         DECLARE y_squared_sum FLOAT;
-         DECLARE xy_sum FLOAT;
-         DECLARE n INT;
-         
-         SELECT COUNT(*) INTO n FROM Market_Data WHERE InvestmentID = id1 OR InvestmentID = id2;
-         
-         IF n < 2 THEN
-             SELECT 'Not enough data points available' AS Error;
-         ELSE
-             SELECT SUM(CASE WHEN InvestmentID = id1 THEN StockPrice ELSE 0 END),
-                    SUM(CASE WHEN InvestmentID = id2 THEN StockPrice ELSE 0 END),
-                    SUM(CASE WHEN InvestmentID = id1 THEN StockPrice*StockPrice ELSE 0 END),
-                    SUM(CASE WHEN InvestmentID = id2 THEN StockPrice*StockPrice ELSE 0 END),
-                    SUM(CASE WHEN InvestmentID = id1 THEN StockPrice ELSE 0 END * CASE WHEN InvestmentID = id2 THEN StockPrice ELSE 0 END)
-             INTO x_sum, y_sum, x_squared_sum, y_squared_sum, xy_sum
-             FROM Market_Data
-             WHERE InvestmentID = id1 OR InvestmentID = id2;
-     
-             SET x = (n * xy_sum - x_sum * y_sum) / (n * x_squared_sum - x_sum * x_sum);
-             SET y = (y_sum - x * x_sum) / n;
-     
-             SELECT x AS Correlation;
-         END IF;
-     END$$
+CREATE PROCEDURE findCorrelation(
+	IN p_investmentID1 INT,
+    IN p_investmentID2 INT
+)
+BEGIN
+	SELECT CORR(pm1.AnnualizedReturn, pm2.AnnualizedReturn) AS Correlation FROM Performance_Metrics pm1 JOIN Performance_Metrics pm2 ON pm1.PerformanceMetricsID <> pm2.PerformanceMetricsID WHERE pm1.InvestmentID = p_investmentID1 AND pm2.InvestmentID = p_investmentID2;
+END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE getInflationRate()
 BEGIN
-	-- Return the most recent inflation rate from the Other_Financial_Information table
-	SELECT Inflation_Rate FROM Other_Financial_Information ORDER BY Date DESC LIMIT 1;
+  -- Return the most recent inflation rate from the Other_Financial_Information table
+  SELECT Inflation_Rate FROM Other_Financial_Information ORDER BY Date DESC LIMIT 1;
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE filterStockPrice(
 	IN p_investmentID INT,
-	IN p_date1  DATE,
-	IN p_date2 DATE
+    IN p_date1  DATE,
+    IN p_date2 DATE
 )
 BEGIN	
 	SELECT StockPrice FROM Market_Data WHERE InvestmentID = p_investmentID AND Date BETWEEN p_date1 AND p_date2;
 END$$
 DELIMITER ;
 
-
 DELIMITER $$
-CREATE PROCEDURE percentageChangeInStockPriceByName(
-    IN p_investmentName VARCHAR(255),
-    IN p_dateOld DATE,
+CREATE PROCEDURE percentageChangeInStockPrice(
+	IN p_investmentID INT,
+    IN p_dateOld  DATE,
     IN p_dateNew DATE
 )
-BEGIN    
-    SELECT (((SELECT StockPrice FROM Market_Data md JOIN Investment i ON md.InvestmentID = i.InvestmentID WHERE i.InvestmentName = p_investmentName AND InvDate = p_dateNew) - (SELECT StockPrice FROM Market_Data md JOIN Investment i ON md.InvestmentID = i.InvestmentID WHERE i.InvestmentName = p_investmentName AND InvDate = p_dateOld)) / (SELECT StockPrice FROM Market_Data md JOIN Investment i ON md.InvestmentID = i.InvestmentID WHERE i.InvestmentName = p_investmentName AND InvDate = p_dateOld)) * 100 AS percentage_change;
+BEGIN	
+	SELECT (((SELECT StockPrice FROM Market_Data WHERE InvestmentID = p_invextmentID AND InvDate = p_dateNew) - (SELECT StockPrice FROM Market_Data WHERE InvestmentID = p_invextmentID AND InvDate = p_oldNew)) / (SELECT StockPrice FROM Market_Data WHERE InvestmentID = p_invextmentID AND InvDate = p_dateOld)) * 100 AS percentage_change FROM Market_Data WHERE InvestmentID = p_investmentID AND Date = p_dateNew UNION ALL SELECT (((SELECT StockPrice FROM Market_Data WHERE InvestmentID = p_invextmentID AND InvDate = p_dateNew) - (SELECT StockPrice FROM Market_Data WHERE InvestmentID = p_invextmentID AND InvDate = p_dateOld)) / (SELECT StockPrice FROM Market_Data WHERE InvestmentID = p_invextmentID AND InvDate = p_dateOld)) * 100 AS percentage_change FROM Market_Data WHERE InvestmentID = p_investmentID AND InvDate = (SELECT StockPrice FROM Market_Data WHERE InvestmentID = p_invextmentID AND InvDate = p_dateOld);
 END$$
 DELIMITER ;
-
-
-
 
 DELIMITER $$
 CREATE PROCEDURE calculateVolatility(
-	IN p_investmentName VARCHAR(255)
+	IN p_investmentID INT
 )
 BEGIN	
-	SET @query = CONCAT('SELECT STDDEV(pm.TotalReturn) AS Volatility FROM Performance_Metrics pm JOIN Investment i ON pm.InvestmentId = i.InvestmentId WHERE i.InvestmentName = ''', p_investmentName, ''';');
-	
-	PREPARE stmt FROM @query;
-	EXECUTE stmt;
-	DEALLOCATE PREPARE stmt;
+	SELECT STDDEV(TotalReturn) as Volatility FROM Performance_Metrics WHERE InvestmentID = p_investmentID;
 END$$
 DELIMITER ;
-
 
 DELIMITER $$
 CREATE PROCEDURE getTopPerformingStocks()
@@ -229,3 +211,8 @@ BEGIN
 	SELECT InvestmentType, SUM(NumShares) AS TotalShares FROM Investment GROUP BY InvestmentType;
 END$$
 DELIMITER ;
+
+
+
+
+With these queries, write 5 call statements to add investors
